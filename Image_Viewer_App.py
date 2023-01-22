a0310f585b2d39f73c015f1bf445892bbe0f137c
ceoyegetaneh <lijnati3@gmail.com>
2023-01-22 15:27:58 +0400

    Initial

---
+++ Image_Viewer_App.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import filedialog
+import tkinter as tk
+from PIL import Image,ImageTk
+import os
+
+
+def showimage():
+    filename=filedialog.askopenfilename(initialdir=os.getcwd(),title="Select Image File",filetype=(("JPG File","*.jpg"),("PNG File","*.png"),("All file", "how are you.txt")))
+    img = Image.open(filename)
+    img = ImageTk.PhotoImage(img)
+    lbl.configure(image=img)
+    lbl.image=img
+
+root=Tk()
+
+fram = Frame(root)
+fram.pack(side=BOTTOM,padx=15,pady=15)
+
+
+lbl=Label(root)
+lbl.pack()
+
+
+btn = Button(fram,text="Select Image",command=showimage)
+btn.pack(side=tk.LEFT)
+
+btn2 = Button(fram,text="Exit",command=lambda:exit())
+btn2.pack(side=tk.LEFT,padx=12)
+
+root.title("Image Viewer")
+root.geometry("400*450")
+root.mainloop()
